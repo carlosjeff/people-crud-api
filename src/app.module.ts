050c3649e51611ce78cda9db71a52111c8c7ce35
CarlosJeffersonBragaAlves <carlos_jefferson_braga@outlook.com>
2022-10-11 23:56:52 -0400

    Setting environmental variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PeopleModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'containers-us-west-39.railway.app',
+    //   port: 5924,
+    //   username: 'root',
+    //   password: '6gMgvg9U6MWHmAKUA77M',
+    //   database: 'railway',
+    //   entities: [People],
+    //   synchronize: true
+    // })
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Cjb@2015',
-      database: 'people',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [People],
       synchronize: true
     })
@@ -26,3 +38,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   ],
 })
 export class AppModule { }
+
+// MYSQLDATABASE=railway
+// MYSQLHOST=containers-us-west-39.railway.app
+// MYSQLPASSWORD=6gMgvg9U6MWHmAKUA77M
+// MYSQLPORT=5924
+// MYSQLUSER=root
